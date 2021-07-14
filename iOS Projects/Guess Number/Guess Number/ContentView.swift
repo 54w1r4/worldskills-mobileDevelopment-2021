ce2139be56ce4d6047dc8e3d4021cd221a3e1f39
Edward <edwardtang0611@gmail.com>
2021-07-14 21:49:38 +0800

    Guess Game

---
+++ iOS Projects/Guess Number/Guess Number/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  Guess Number
+//
+//  Created by in112-05-2021-cm on 14/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let greenBackground = [
+        Color(red: 234.0/255.0, green: 255.0/255.0, blue: 207.0/255.0),
+        Color(red: 135.0/255.0, green: 217.0/255.0, blue: 52.0/255.0)
+    ]
+    
+    var body: some View {
+        
+        ZStack {
+            Rectangle().fill(LinearGradient(gradient: Gradient(colors: greenBackground), startPoint: .top, endPoint: .bottom))
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 25.0) {
+                VStack {
+                    
+                }.padding(.trailing, 52.0)
+                .padding(.leading, 51.0)
+                .frame(maxWidth: 272, maxHeight: 118)
+                .background(Color(red: 83.0/255.0, green: 83.0/255.0, blue: 83.0/255.0))
+                .cornerRadius(20.0)
+                .foregroundColor(.white)
+                
+                VStack {
+                    
+                }.padding(.trailing, 52.0)
+                .padding(.leading, 51.0)
+                .frame(maxWidth: 272, maxHeight: 118)
+                .background(Color.white)
+                .cornerRadius(20.0)
+                .foregroundColor(.white)
+                
+                VStack {
+                    
+                }.padding(.trailing, 52.0)
+                .padding(.leading, 51.0)
+                .frame(maxWidth: 272, maxHeight: 118)
+                .background(Color(red: 83.0/255.0, green: 83.0/255.0, blue: 83.0/255.0))
+                .cornerRadius(20.0)
+                .foregroundColor(.white)
+                
+            }
+            
+            
+            
+        }
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS Projects/Guess Number/Guess Number/Guess_NumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_NumberApp.swift
+//  Guess Number
+//
+//  Created by in112-05-2021-cm on 14/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Guess_NumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
