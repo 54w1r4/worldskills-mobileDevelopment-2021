3ecd1acad14498c7ef9dda8d8019283cb9da5957
Edward <edwardtang0611@gmail.com>
2021-07-14 21:55:37 +0800

    content view

--- iOS Projects/Guess Number/Guess Number/ContentView.swift
+++ iOS Projects/Guess Number/Guess Number/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let maxTries = 5
+    
     let greenBackground = [
         Color(red: 234.0/255.0, green: 255.0/255.0, blue: 207.0/255.0),
         Color(red: 135.0/255.0, green: 217.0/255.0, blue: 52.0/255.0)
@@ -17,38 +19,51 @@ struct ContentView: View {
     var body: some View {
         
         ZStack {
+            
             Rectangle().fill(LinearGradient(gradient: Gradient(colors: greenBackground), startPoint: .top, endPoint: .bottom))
                 .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
                 .edgesIgnoringSafeArea(.all)
             
+            
             VStack(spacing: 25.0) {
-                VStack {
-                    
-                }.padding(.trailing, 52.0)
-                .padding(.leading, 51.0)
-                .frame(maxWidth: 272, maxHeight: 118)
-                .background(Color(red: 83.0/255.0, green: 83.0/255.0, blue: 83.0/255.0))
-                .cornerRadius(20.0)
-                .foregroundColor(.white)
                 
-                VStack {
-                    
-                }.padding(.trailing, 52.0)
-                .padding(.leading, 51.0)
-                .frame(maxWidth: 272, maxHeight: 118)
-                .background(Color.white)
-                .cornerRadius(20.0)
-                .foregroundColor(.white)
+                HStack {
+                    ForEach(0 ..< maxTries) { index in
+                        Image(systemName: "heart.fill")
+                            .foregroundColor(.red)
+                    }
+                }
                 
-                VStack {
-                    
-                }.padding(.trailing, 52.0)
-                .padding(.leading, 51.0)
-                .frame(maxWidth: 272, maxHeight: 118)
-                .background(Color(red: 83.0/255.0, green: 83.0/255.0, blue: 83.0/255.0))
-                .cornerRadius(20.0)
-                .foregroundColor(.white)
+                Spacer()
                 
+                    VStack {
+                    
+                    }.padding(.trailing, 52.0)
+                    .padding(.leading, 51.0)
+                    .frame(maxWidth: 272, maxHeight: 118)
+                    .background(Color(red: 83.0/255.0, green: 83.0/255.0, blue: 83.0/255.0))
+                    .cornerRadius(20.0)
+                    .foregroundColor(.white)
+                
+                    VStack {
+                    
+                    }.padding(.trailing, 52.0)
+                    .padding(.leading, 51.0)
+                    .frame(maxWidth: 272, maxHeight: 118)
+                    .background(Color.white)
+                    .cornerRadius(20.0)
+                    .foregroundColor(.white)
+                
+                    VStack {
+                    
+                    }.padding(.trailing, 52.0)
+                    .padding(.leading, 51.0)
+                    .frame(maxWidth: 272, maxHeight: 118)
+                    .background(Color(red: 83.0/255.0, green: 83.0/255.0, blue: 83.0/255.0))
+                    .cornerRadius(20.0)
+                    .foregroundColor(.white)
+                
+                Spacer()
             }
             
             
